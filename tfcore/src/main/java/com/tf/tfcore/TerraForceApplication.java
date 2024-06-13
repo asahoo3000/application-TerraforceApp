2813371a5f926c5f230ee300ebefebcef9ee3ee6
Ashu <asahoo3000@gmail.com>
2024-06-13 04:58:08 -0700

    Initial commit

---
+++ tfcore/src/main/java/com/tf/tfcore/TerraForceApplication.java
@@ -0,0 +1,13 @@
+package com.tf.tfcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerraForceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerraForceApplication.class, args);
+	}
+
+}
---
+++ tfcore/src/test/java/com/tf/tfcore/TerraForceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tf.tfcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerraForceApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
